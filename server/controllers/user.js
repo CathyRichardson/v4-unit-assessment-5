e454c073d204589ef4e31d6b6814f0bc2bc6d84c
Cathy Richardson <cathyanne08@gmail.com>
2021-08-31 20:50:40 -0600

    finished authorization

---
+++ server/controllers/user.js
@@ -0,0 +1,60 @@
+const bcrypt = require('bcryptjs');
+
+
+const register = async (req, res) => {
+    const { username, password } = req.body;
+    const profile_pic = `https://robohash.org/${username}.png`;
+    const db = req.app.get('db');
+    const result = await db.user.find_user_by_username(username);
+    const existingUser = result[0];
+    if (existingUser) {
+        return res.status(409).send('Username taken');
+    }
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    const registeredUser = await db.user.create_user(username, hash, profile_pic);
+    const user = registeredUser[0];
+    req.session.user = { username: user.username, id: user.id, profile_pic: user.profile_pic };
+    return res.status(201).send(req.session.user);
+}
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    const db = req.app.get('db');
+    const foundUser = await db.user.find_user_by_username(username);
+    const user = foundUser[0];
+    if (!user) {
+        return res.status(401).send('User  not found. Please register as a new user before logging in.');
+    }
+    const isAuthenticated = bcrypt.compareSync(password, user.password);
+    if (!isAuthenticated) {
+        return res.status(403).send('Incorrect password');
+    }
+    req.session.user = { username: user.username, id: user.id, profile_pic: user.profile_pic };
+    return res.send(req.session.user);
+}
+
+const logout = (req, res) => {
+    req.session.destroy();
+    return res.sendStatus(200);
+}
+
+const usersOnly = (req, res, next) => {
+    if (!req.session.user) {
+        return res.status(404).send('Please log in');
+    }
+    next();
+}
+
+const getUser = async (req, res) => {
+    const { user } = req.session;
+    return res.status(200).send(user);
+}
+
+module.exports = {
+    register,
+    login,
+    logout,
+    usersOnly,
+    getUser
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const massive = require('massive');
-const { CONNECTION_STRING, SERVER_PORT } = process.env;
+const session = require('express-session');
+const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env;
 const express = require('express'),
       userCtrl = require('./controllers/user'),
       postCtrl = require('./controllers/posts')
@@ -9,6 +10,14 @@ const express = require('express'),
 const app = express();
 
 app.use(express.json());
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 90,
+    }
+}))
 
 massive({
     connectionString: CONNECTION_STRING,
@@ -24,13 +33,13 @@ massive({
 //Auth Endpoints
 app.post('/api/auth/register', userCtrl.register);
 app.post('/api/auth/login', userCtrl.login);
-app.get('/api/auth/me', userCtrl.getUser);
+app.get('/api/auth/me', userCtrl.usersOnly, userCtrl.getUser);
 app.post('/api/auth/logout', userCtrl.logout);
 
 //Post Endpoints
-app.get('/api/posts', postCtrl.readPosts);
-app.post('/api/post', postCtrl.createPost);
-app.get('/api/post/:id', postCtrl.readPost);
-app.delete('/api/post/:id', postCtrl.deletePost)
+// app.get('/api/posts', postCtrl.readPosts);
+// app.post('/api/post', postCtrl.createPost);
+// app.get('/api/post/:id', postCtrl.readPost);
+// app.delete('/api/post/:id', postCtrl.deletePost)
 
 app.listen(SERVER_PORT, () => console.log(`running on ${SERVER_PORT}`));
